3ed4b9256560cec10f12963936940a1b53b693e5
qunox <mohd.adli.bin.md.ali@cern.ch>
2014-08-26 15:21:15 +0200

    fix #4136 upload automatically the log file for unhandled exception

--- src/python/CRABClient/Commands/uploadlog.py
+++ src/python/CRABClient/Commands/uploadlog.py
@@ -4,6 +4,7 @@ from WMCore.Services.UserFileCache.UserFileCache import UserFileCache
 from CRABClient.client_utilities import colors
 from CRABClient.client_utilities import server_info
 from CRABClient.client_exceptions import ConfigurationException
+from CRABClient.client_utilities import uploadlogfile
 import time
 
 class uploadlog(SubCommand):
@@ -27,21 +28,7 @@ class uploadlog(SubCommand):
             self.logger.info("%sError:%s Could not locate log file" % (colors.RED, colors.NORMAL))
             raise ConfigurationException
 
-        #getting the cache url
-        baseurl=self.getUrl(self.instance, resource='info')
-        cacheurl=server_info('backendurls', self.serverurl, self.proxyfilename, baseurl)
-        cacheurl=cacheurl['cacheSSL']
-        cacheurldict={'endpoint' : cacheurl}
-
-        ufc=UserFileCache(cacheurldict)
-        self.logger.debug("cacheURL: %s\nLog file name: %s" % (cacheurl, logfilename))
-        self.logger.info("Uploading log file")
-        ufc.uploadLog(str(self.logfile), logfilename)
-
-        self.logger.info("%sSuccess%s: Finish uploading log file" % (colors.GREEN, colors.NORMAL))
-        logfileurl = cacheurl + '/logfile?name='+str(logfilename)
-        self.logger.info("Log file url: %s" %logfileurl)
-
+        logfileurl = uploadlogfile(self.logger, self.proxyfilename, logfilename = logfilename, logpath = str(self.logfile), instance = self.instance)
 
     def setOptions(self):
         """
@@ -60,27 +47,29 @@ class uploadlog(SubCommand):
                                      default = None,
                                      help = "Same as -c/-continue" )
 
-
     def validateOptions(self):
         SubCommand.validateOptions(self)
 
         if hasattr(self.options, 'logpath') and self.options.logpath != None:
+            self.requiresTaskOption =  False
             if not path.isfile(self.options.logpath):
                 msg = '%sError%s: Could not find the log file in the path: %s' % (colors.RED,colors.NORMAL,self.options.logpath)
                 raise ConfigurationException(msg)
-        elif hasattr(self.options, 'task'):
-            self.cmdconf['requiresTaskOption'] = True
-            self.cmdconf['useCache'] = True
-            if self.options.task == None:
-                if len(self.args) == 1 and self.args[0]:
-                    self.options.task = self.args[0]
-                #for the case of 'crab uploadlog', user is using the .crab3 file
-                if self.options.task == None and hasattr(self, 'crab3dic'):
-                    if  self.crab3dic["taskname"] != None:
-                        self.options.task = self.crab3dic["taskname"]
-                    else:
-                        msg = '%sError%s: Please use the task option -t or --logpath to specify which log to upload' % (colors.RED, colors.NORMAL)
-                        raise ConfigurationException(msg)
+
+        elif hasattr(self.options, 'task') and self.options.task == None:
+            self.requiresTaskOption = True
+            if len(self.args) == 1 and self.args[0]:
+                self.options.task = self.args[0]
+            #for the case of 'crab uploadlog', user is using the .crab3 file
+            if self.options.task == None and hasattr(self, 'crab3dic'):
+                if  self.crab3dic["taskname"] != None:
+                    self.options.task = self.crab3dic["taskname"]
+                    self.requiresTaskOption = True
+                else:
+                    msg = '%sError%s: Please use the task option -t or --logpath to specify which log to upload' % (colors.RED, colors.NORMAL)
+                    raise ConfigurationException(msg)
+        elif self.options.task != None:
+            self.requiresTaskOption = True
         else:
             msg = '%sError%s: Please use the task option -t or --logpath to specify which log to upload' % (colors.RED, colors.NORMAL)
             raise ConfigurationException(msg)
